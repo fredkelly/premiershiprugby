b58b7c98c139fdbae4a33e727e81f6a5eb8c3571
Freddy Kelly <fred@bitnami.com>
2014-10-24 15:21:28 -0700

    basic class/method stubs

---
+++ premiershiprugby.rb
@@ -0,0 +1,18 @@
+module PremiershipRugby
+
+  SOURCE_URL = "http://www.premiershiprugby.tv/"
+
+  class Client
+
+    # Scrape SOURCE_URL and return Replay instances
+
+  end
+
+
+  class Replay
+
+    # Initialise from xml, provide accessors for each media URL
+
+  end
+
+end
