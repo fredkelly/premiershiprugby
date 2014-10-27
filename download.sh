7ab78ac25e7112b31eadbd5a0219bd0549a09355
Freddy Kelly <me@fredkelly.net>
2014-10-26 21:42:45 -0700

    redirect stderr to logfile

--- download.sh
+++ download.sh
@@ -2,4 +2,4 @@ RTMP_LOG=./rtmpdump.log
 TARGET_DIR=/mnt/storage/premiershiprugby/
 
 touch $RTMP_LOG
-./premiershiprugby.rb download --quality=high --formats=.flv --target=$TARGET_DIR --preview=false --skip=1 >> $RTMP_LOG
+./premiershiprugby.rb download --quality=high --formats=.flv --target=$TARGET_DIR --preview=false --skip=1 &>> $RTMP_LOG
