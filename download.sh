4b3e9b6abb1481d1d79d7bc58da3093a8447f493
Freddy Kelly <me@fredkelly.net>
2014-10-26 22:02:29 -0700

    remove logging for now

--- download.sh
+++ download.sh
@@ -1,5 +1,3 @@
-LOG_FILE=./download.log
 TARGET_DIR=/mnt/storage/premiershiprugby/
 
-touch $RTMP_LOG
-./premiershiprugby.rb download --quality=high --formats=.flv --target=$TARGET_DIR --preview=false --skip=1 &>> $LOG_FILE
+./premiershiprugby.rb download --quality=high --formats=.flv --target=$TARGET_DIR --preview=false --skip=1
