fae311c7291010983a058483b41a3c328e28d46e
Freddy Kelly <me@fredkelly.net>
2014-10-26 21:43:15 -0700

    renamed logfile

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
-RTMP_LOG=./rtmpdump.log
+LOG_FILE=./download.log
 TARGET_DIR=/mnt/storage/premiershiprugby/
 
 touch $RTMP_LOG
-./premiershiprugby.rb download --quality=high --formats=.flv --target=$TARGET_DIR --preview=false --skip=1 &>> $RTMP_LOG
+./premiershiprugby.rb download --quality=high --formats=.flv --target=$TARGET_DIR --preview=false --skip=1 &>> $LOG_FILE
