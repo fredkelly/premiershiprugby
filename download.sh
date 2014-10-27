1bfda4cc5f2f2c4d8936623a3997c3ea7d09998e
Freddy Kelly <me@fredkelly.net>
2014-10-26 18:41:00 -0700

    adds logging of rtmp queries

--- download.sh
+++ download.sh
@@ -1,2 +1,5 @@
+RTMP_LOG=./rtmpdump.log
 TARGET_DIR=/mnt/storage/premiershiprugby/
-./premiershiprugby.rb download --quality=high --formats=.flv --target=$TARGET_DIR --preview=false --skip=1
+
+touch $RTMP_LOG
+./premiershiprugby.rb download --quality=high --formats=.flv --target=$TARGET_DIR --preview=false --skip=1 >> $RTMP_LOG
