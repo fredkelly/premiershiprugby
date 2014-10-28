e17545b8479990290bcdb649daa8ae538df64312
Freddy Kelly <murica@fredkelly.net>
2014-10-28 05:40:19 +0000

    lifts limit to 50 matches

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 LOG_FILE=./download.log
 TARGET_DIR=/mnt/storage/premiershiprugby/
 
-( ./premiershiprugby.rb download --quality=high --formats=.flv --target=$TARGET_DIR --no-preview --skip=1 --limit=1 ) 2>> $LOG_FILE
+( ./premiershiprugby.rb download --quality=high --formats=.flv --target=$TARGET_DIR --no-preview --skip=1 --limit=50 ) 2>> $LOG_FILE
